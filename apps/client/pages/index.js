6431a7500585750c7502b642fb02d12aa503d939
robylosa94 <roby94.losa@tiscali.it>
2023-08-10 10:55:09 +0200

    Edit client app

--- apps/client/pages/index.js
+++ apps/client/pages/index.js
@@ -1,7 +1,7 @@
-import { Header } from 'ui';
+import { Header } from 'ui'
 
 const Home = () => {
-  return <Header>client</Header>
+  return <Header>client!!!</Header>
 }
 
-export default Home;
+export default Home
